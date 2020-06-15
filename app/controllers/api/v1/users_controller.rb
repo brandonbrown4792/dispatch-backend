ca45c24454fab020bae0221799cb2bb75248c5b7
github username <github email address>
2020-06-15 08:16:41 -0400

    Added notes to appointments json return for nurses and patients

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -25,14 +25,14 @@ class Api::V1::UsersController < ApplicationController
         email: current_user.email,
         user_type: user_type,
         patients: current_user.patients.as_json(only: [:id, :name, :email, :address, :latitude, :longitude]),
-        appointments: current_user.nurse_appointments.as_json(except: [:created_at, :updated_at])
+        appointments: current_user.nurse_appointments.as_json(except: [:created_at, :updated_at], include: [notes: {only: :content}])
       }, status: :ok
     elsif user_type == 'patient'
       render json: {
         email: current_user.email,
         user_type: user_type,
         nurses: current_user.nurses.as_json(only: [:id, :name, :email]),
-        appointments: current_user.patient_appointments.as_json(except: [:created_at, :updated_at])
+        appointments: current_user.patient_appointments.as_json(except: [:created_at, :updated_at], include: [:notes])
       }, status: :ok
     end
   end
