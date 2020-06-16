93ce865bc1e3dd05c39354a8f49dc271d1bc3ca4
rpdecks <rpdecks@gmail.com>
2020-06-15 20:21:37 -0400

    appt edits

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,3 +1,18 @@
 class Api::V1::AppointmentsController < ApplicationController
-  before_action :authenticate, only: [:index]
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    render json: @appointment, status: ok
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:patient_id, :nurse_id, :address, :start_time, :length, :reason, :notes)
+  end
+
+  def find_appintment
+    @user = Appointment.find_by(id: params[:id])
+  end
 end
+
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment < ApplicationRecord
   has_many :notes
   belongs_to :patient, class_name: 'User'
-  belongs_to :nurse, class_name: 'User'
+  belongs_to :nurse, class_name: 'User', optional: true
 end
